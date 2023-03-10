d7b03aaa13d2634b0b1ccbfdd4bfc1e0c18a9c26
hernan debenedetto <hernan.debenedetto@hotmail.com>
2023-03-09 23:01:23 -0300

    si

---
+++ src/main/java/com/market/detoMarket/DetoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.detoMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/detoMarket/DetoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.market.detoMarket;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DetoMarketApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
