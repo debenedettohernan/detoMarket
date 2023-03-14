127ce36447754dfb37b8fa205a2a5634d59030bf
hernan debenedetto <hernan.debenedetto@hotmail.com>
2023-03-13 22:45:13 -0300

    creacion de la estructura del proyecto

--- src/main/java/com/market/detoMarket/web/controller/DetoMarketApplication.java
+++ src/main/java/com/market/detoMarket/web/controller/DetoMarketApplication.java
@@ -1,4 +1,4 @@
-package com.market.detoMarket;
+package com.market.detoMarket.web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
